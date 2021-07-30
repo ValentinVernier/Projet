04db7f1a1b9dcad14c31ceda9aa26bf28eb9f2a5
ValentinVernier <63792187+ValentinVernier@users.noreply.github.com>
2021-07-30 11:14:13 +0200

    Création nav- modif version bootstrap
    passage à la version 4.6.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Projet';
+  title:string = 'Projet Angular';
 }
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,6 +10,7 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log("NgOnInit du header")
   }
 
 }
