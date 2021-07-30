2940c8f9eb966709f30c094432e6a4e62df73adf
ValentinVernier <63792187+ValentinVernier@users.noreply.github.com>
2021-07-30 13:40:11 +0200

    Ajout de deux component
    driversList et carsList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CarsListComponent } from './cars-list/cars-list.component';
+import { DriversListComponent } from './drivers-list/drivers-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CarsListComponent,
+    DriversListComponent
   ],
   imports: [
     BrowserModule,
