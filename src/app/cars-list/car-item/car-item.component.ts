de5c8033f3c9c9a969b71dafdb75d8a576228f04
ValentinVernier <63792187+ValentinVernier@users.noreply.github.com>
2021-08-03 12:34:26 +0200

    Custom property binding

--- src/app/cars-list/car-item/car-item.component.ts
+++ src/app/cars-list/car-item/car-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-car-item',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-item.component.css']
 })
 export class CarItemComponent implements OnInit {
+
+  vehicule:string = "ferrari";
+
+  @Input() enfant:any;
 
   constructor() { }
 
