831639d642896753a1992843c66d3601ad17f839
ValentinVernier <63792187+ValentinVernier@users.noreply.github.com>
2021-08-03 10:02:27 +0200

    interpolation exemple

--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsListComponent implements OnInit {
 
+
+  // exemples d'interpolation
+  mardiMatin:any = "Ils ont l'air bien OFF";
+  age:number = 13;
+
+  //2eme exemple objet ou propriété
+    game:any = {
+      title:"BF 2042",
+      support: "Xbox Serie X",
+      multi: 128
+    };
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // Interpolation du retour d'une méthode
+  getStock(){
+    return 2050;
+  }
+
+  
 
 }
