3903f297ac269e28af4c0b76e60c11d1cbcb575c
ValentinVernier <63792187+ValentinVernier@users.noreply.github.com>
2021-08-10 15:54:12 +0200

    Créations méthodes
    getAllCars et getAllDrivers

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
   title:string = "Drive-X"
+  
   constructor() { }
 
   ngOnInit(): void {
